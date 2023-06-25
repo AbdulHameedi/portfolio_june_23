9dd4af9203b6c6b337f38a54c08590b0cfc84c08
AbdulHameedi <sanilinity507@gmail.com>
2023-06-25 23:25:54 +0100

    testimonial

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,6 +11,36 @@ let hamburger = document.querySelector(".hamburger")
 let cancelSidebar = document.querySelector(".cancel-sidebar")
 let sidebar = document.querySelector(".sidebar")
 let overlay = document.querySelector(".overlay")
+let testimony = document.querySelector(".testimony")
+let names = document.querySelector(".name")
+let forward = document.querySelector(".forward")
+let current = 0
+let testimonials = [
+    {
+        "id": "0",
+        "name": "Sanni Hammed",
+        "job": "Software Developer",
+        "grade": "Excellent",
+        "speech": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, similique assumenda quis eligendi, aliquam optio voluptate at animi facere incidunt tempore minima ipsa vero. Nemo consequuntur maxime nihil beatae rem?",
+        "project": "Team Collaboration - Software"
+    },
+    {
+        "id": "0",
+        "name": "Abdulrahman Luqman",
+        "job": "Software Developer",
+        "grade": "VERY GOOD",
+        "speech": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, similique assumenda quis eligendi, aliquam optio voluptate at animi facere incidunt tempore minima ipsa vero. Nemo consequuntur maxime nihil beatae rem?",
+        "project": "Team Collaboration - Software"
+    },
+    {
+        "id": "0",
+        "name": "Anjorin Muhsin",
+        "job": "Chief Technology Officer",
+        "grade": "Excellent",
+        "speech": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, similique assumenda quis eligendi, aliquam optio voluptate at animi facere incidunt tempore minima ipsa vero. Nemo consequuntur maxime nihil beatae rem?",
+        "project": "Top Notch Solution - Clients"
+    },
+]   
 
 
 // show sidebar
@@ -30,3 +60,19 @@ cancelSidebar.addEventListener("click" , cancel)
 // clicking overlay
 overlay.addEventListener("click" , cancel)
 
+// for testimonials
+
+
+// testimonials
+for(i in testimonials){
+    if(testimonials[i] === testimonials[current]){
+        names.innerHTML = testimonials[i].name
+    }
+    console.log(testimonials[i].name)
+}
+
+forward.addEventListener("click" , incCurrent)
+function incCurrent(){
+    current = current+1
+    console.log(current)
+}
