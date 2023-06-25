524b872b9706393a325087ec1b62f0e2324d9ac5
AbdulHameedi <sanilinity507@gmail.com>
2023-06-25 15:42:27 +0100

    portfolio

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,3 +6,32 @@ let typed = new Typed(".auto-type" , {
     backSpeed: 70,
     loop:true
 });
+
+let hamburger = document.querySelector(".hamburger")
+let cancelButton = document.querySelector(".cancel-button")
+
+function hamburgerDisplay(){
+    hamburger.classList.add('hide')
+    cancelButton.classList.add('show')
+}
+hamburger.addEventListener("click" , hamburgerDisplay)
+
+
+
+function cancelDisplay(){
+    hamburger.classList.add('hide')
+    cancelButton.classList.remove('hide')
+    cancelButton.classList.add('show')
+}
+cancelButton.addEventListener("click" , cancelDisplay)
+// hamburger click
+// hamburger.onclick = function(){
+//     hamburger.classList.add('hide')
+//     cancelButton.classList.add('show')
+// }
+
+// // cancel click
+// cancelButton.onclick = function(){
+//     cancelButton.classList.add('hide')
+//     hamburger.classList.remove('hide')
+// }
